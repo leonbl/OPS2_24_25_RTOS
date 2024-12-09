2a75e0a90ee515febe1a4062c533c9c7f941e031
leonbl <leon.blazic@gmail.com>
2024-12-09 11:41:06 +0100

    Update main.cpp

--- RTOS4/src/main.cpp
+++ RTOS4/src/main.cpp
@@ -4,19 +4,14 @@ SemaphoreHandle_t semafor;
 
 void blink1(void *pvParameters);
 void blink2(void *pvParameters);
-void blink3(void *pvParameters);
-void blink4(void *pvParameters);
+
 void setup() {
   pinMode(16, OUTPUT);
   pinMode(17, OUTPUT);
-  pinMode(18, OUTPUT);
-  pinMode(19, OUTPUT);
-  semafor = xSemaphoreCreateCounting(6,6);
+
+  semafor = xSemaphoreCreateBinary();
   xTaskCreate(blink1, "blink1", 2048, NULL, 1, NULL);
   xTaskCreate(blink2, "blink2", 2048, NULL, 1, NULL);
-  xTaskCreate(blink3, "blink3", 2048, NULL, 1, NULL);
-    xTaskCreate(blink4, "blink3", 2048, NULL, 1, NULL);
-  xSemaphoreGive(semafor);
 }
 
 void loop() {
@@ -24,65 +19,34 @@ void loop() {
 }
 
 void blink1(void *pvParameters){
-  int le1 = 0;
   while(1){
-    le1 = random(16,20);
-    xSemaphoreTake(semafor,portMAX_DELAY);
-    for(int n = 0; n<random(1,4); n++){
-      digitalWrite(le1, 1);
-      delay(100*random(1,10));
-      digitalWrite(le1, 0);
-      delay(100*random(1,10));
+    for(int n = 0; n<5; n++){
+      digitalWrite(16, 1);
+      delay(100);
+      digitalWrite(16, 0);
+      delay(100);
     }
     xSemaphoreGive(semafor);
+    for(int n = 0; n<5; n++){
+      digitalWrite(16, 1);
+      delay(50);
+      digitalWrite(16, 0);
+      delay(50);
+    }
     delay(10);
   }
 }
 
 void blink2(void *pvParameters){
-  int le2 = 0;
   while(1){
-    le2 = random(16,20);
-    xSemaphoreTake(semafor,portMAX_DELAY);
-    for(int n = 0; n<random(1,4); n++){
-      digitalWrite(le2, 1);
-      delay(100*random(1,10));
-      digitalWrite(le2, 0);
-      delay(100*random(1,10));
+    xSemaphoreTake(semafor, portMAX_DELAY);
+    for(int n = 0; n<5; n++){
+      digitalWrite(17, 1);
+      delay(100);
+      digitalWrite(17, 0);
+      delay(100);
     }
-    xSemaphoreGive(semafor);
-    delay(10);
-  }
-}
-
-void blink3(void *pvParameters){
-  int le3 = 0;
-  while(1){
-    le3 = random(16,20);
-    xSemaphoreTake(semafor,portMAX_DELAY);
-    for(int n = 0; n<random(1,4); n++){
-      digitalWrite(le3, 1);
-      delay(100*random(1,10));
-      digitalWrite(le3, 0);
-      delay(100*random(1,10));
-    }
-    xSemaphoreGive(semafor);
-    delay(10);
-  }
-}
-
-void blink4(void *pvParameters){
-  int le3 = 0;
-  while(1){
-    le3 = random(16,20);
-    xSemaphoreTake(semafor,portMAX_DELAY);
-    for(int n = 0; n<random(1,4); n++){
-      digitalWrite(le3, 1);
-      delay(100*random(1,10));
-      digitalWrite(le3, 0);
-      delay(100*random(1,10));
-    }
-    xSemaphoreGive(semafor);
+    
     delay(10);
   }
 }
